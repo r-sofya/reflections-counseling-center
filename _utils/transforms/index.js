fa50725e88a8db7ab979c25c7fdb7634d2ceed25
r-sofya <rani.sofya@trulycustom.ca>
2025-04-10 23:33:19 -0400

    Files committed from Adapter on Thu Apr 10 2025

--- _utils/transforms/index.js
+++ _utils/transforms/index.js
@@ -2,7 +2,8 @@ const htmlmin = require("html-minifier");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-        if (outputPath.endsWith(".html")) {
+        return content;
+       /* if (outputPath.endsWith(".html")) {
             try {
                 let minified = htmlmin.minify(content, {
                     useShortDoctype: true,
@@ -15,6 +16,6 @@ module.exports = function(eleventyConfig) {
             }
         }
 
-        return content;
+        return content;*/
     });
 }
